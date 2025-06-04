24b1aa1d245d0efae494123fea25d05f0c8de3f8
MrNDN-24 <ducnhan279notionktkt279@gmail.com>
2025-06-04 14:07:28 +0700

    final

--- toxic_api.py
+++ toxic_api.py
@@ -4,7 +4,7 @@ import torch
 import numpy as np
 import os
 import gdown
-from pyvi import ViTokenizer
+
 
 if not os.path.exists("save_weights.pt"):
     file_id = "1HnfoPRQGPwD6ivtufCR_HpKuVghmdz3T"  
@@ -20,21 +20,19 @@ model.eval()
 app = Flask(__name__)
 
 def predict_toxic(sentence):
-    # Dùng pyvi để tách từ
-    sentence = ViTokenizer.tokenize(sentence)
-
-    # Encode và pad
+    tokens = rdrsegmenter.tokenize(sentence)
+    statement = ""
+    for token in tokens:
+        statement += " ".join(token)
+    sentence = statement
     sequence = tokenizer.encode(sentence)
-    while len(sequence) == 20:  # sửa lại điều kiện cho đúng logic pad
+    while len(sequence) == 20:
         sequence.insert(0, 0)
     padded = torch.tensor([sequence])
-
     with torch.no_grad():
         preds = model(padded)
-
     preds = np.argmax(preds.cpu().numpy(), axis=1)
     return preds[0]
-
 
 
 @app.route('/predict', methods=['POST'])
@@ -48,5 +46,5 @@ def predict():
     return jsonify({'toxic': int(result)})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5001))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
